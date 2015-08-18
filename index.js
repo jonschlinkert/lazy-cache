12ad463049180b5a89b7a7c0d86e963ad3ffbdd7
Brian Woodward <brian.woodward@gmail.com>
2015-08-18 13:33:54 -0400

    adding getter properties on the returned lazy function

--- index.js
+++ index.js
@@ -1,23 +1,59 @@
 'use strict';
+var colors = lazyCache(require);
+colors('ansi-yellow');
 
-var warning = lazyCache(require)('ansi-yellow');
+/**
+ * Cache results of the first function call to ensure only calling once.
+ *
+ * ```js
+ * // cache the call to `require('ansi-yellow')`
+ * var lazy = lazyCache(require);
+ * lazy('ansi-yellow');
+ * // use `ansi-yello`
+ * console.log(lazy.ansiYellow('this is yellow'));
+ * ```
+ *
+ * @param  {Function} `fn` Function that will be called only once.
+ * @return {Function} Function that can be called to get the cached function
+ * @api public
+ */
 
 function lazyCache(fn) {
   var cache = {};
-  return function (name) {
-    return function () {
+  var lazy = function (name) {
+    Object.defineProperty(lazy, camelcase(name), {
+      get: getter
+    });
+
+    function getter () {
       if (cache.hasOwnProperty(name)) {
         return cache[name];
       }
       try {
         return (cache[name] = fn(name));
       } catch (err) {
-        console.log(warning()(err));
+        console.log(colors.ansiYellow(err));
         return;
       }
     };
+    return getter;
   };
+  return lazy;
 }
+
+/**
+ * Used to camelcase the name to be stored on the `lazy` object.
+ *
+ * @param  {String} `str` String containing `_`, `.`, `-` or whitespace that will be camelcased.
+ * @return {String} camelcased string.
+ */
+
+function camelcase(str) {
+  return str.replace(/[_.-](\w|$)/g, function (_, first) {
+    return first.toUpperCase();
+  });
+}
+
 
 /**
  * Expose `lazyCache`
---
+++ test.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var assert = require('assert');
+var lazyCache = require('./');
+
+describe('lazy-cache', function () {
+  it('should return a function that lazyily requires a module', function () {
+    var lazy = lazyCache(require);
+    var yellow = lazy('ansi-yellow');
+    assert.deepEqual(typeof yellow, 'function');
+    assert.deepEqual(typeof yellow(), 'function');
+  });
+
+  it('should add a property to the lazy function with the camelcased name', function () {
+    var lazy = lazyCache(require);
+    lazy('ansi-yellow');
+    assert.deepEqual(typeof lazy.ansiYellow, 'function');
+  });
+
+  it('should only require a dependency once', function () {
+    var calls = {};
+    var lazy = lazyCache(function (mod) {
+      calls[mod] = (calls[mod] || 0) + 1;
+      return require(mod);
+    });
+
+    assert.deepEqual(calls, {});
+    lazy('ansi-yellow');
+    assert.deepEqual(calls, {});
+
+    lazy.ansiYellow('one');
+    assert.deepEqual(calls, {'ansi-yellow': 1});
+    lazy.ansiYellow('two');
+    assert.deepEqual(calls, {'ansi-yellow': 1});
+    lazy.ansiYellow('three');
+    assert.deepEqual(calls, {'ansi-yellow': 1});
+    lazy.ansiYellow('four');
+    assert.deepEqual(calls, {'ansi-yellow': 1});
+  });
+});
