fee01006a3b9f14c3dbacf30112eee2eeae22e27
Brian Woodward <brian.woodward@gmail.com>
2016-04-30 23:22:06 -0400

    use set-getter

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var set = require('set-getter');
+
 /**
  * Cache results of the first function call to ensure only calling once.
  *
@@ -37,7 +39,7 @@ function lazyCache(requireFn) {
       getter();
     }
 
-    defineGetter(proxy, key, getter);
+    set(proxy, key, getter);
     return getter;
   };
 }
@@ -48,53 +50,6 @@ function lazyCache(requireFn) {
 
 function unlazy(env) {
   return env.UNLAZY === 'true' || env.UNLAZY === true || env.TRAVIS;
-}
-
-/**
- * Define a `getter` function for `prop` on the `cache` object. Dot notation is
- * supported.
- *
- * @param {Object} `cache`
- * @param {String} `prop`
- * @param {Function} `getter` Getter function
- * @return {Object}
- */
-
-function defineGetter(cache, prop, getter) {
-  if (!~prop.indexOf('.')) {
-    defineProperty(cache, prop, getter);
-    return cache;
-  }
-  var keys = prop.split('.');
-  var last = keys.pop();
-  var proxy = cache;
-  var key;
-
-  while ((key = keys.shift())) {
-    while (key.slice(-1) === '\\') {
-      key = key.slice(0, -1) + '.' + keys.shift();
-    }
-    proxy = proxy[key] || (proxy[key] = {});
-  }
-
-  defineProperty(proxy, last, getter);
-  return cache;
-}
-
-/**
- * Define a `getter` function for `prop` on the `obj`.
- *
- * @param {Object} `obj`
- * @param {String} `prop`
- * @param {Function} `getter` Getter function
- */
-
-function defineProperty(obj, prop, getter) {
-  Object.defineProperty(obj, prop, {
-    configurable: true,
-    enumerable: true,
-    get: getter
-  });
 }
 
 /**
